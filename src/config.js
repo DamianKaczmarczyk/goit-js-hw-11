8628a6401e6e3272d745850e96c1fb23b01b7fdd
DamianKaczmarczyk <jokernick@o2.pl>
2023-09-13 16:00:18 +0200

    test

---
+++ src/config.js
@@ -0,0 +1,10 @@
+export const API_PATH = 'https://pixabay.com/api';
+export const API_KEY = '39342195-5a46dd0258ec48c84a8f82d33';
+
+export const DEFAULT_PIXABAY_PARAMS = {
+  key: API_KEY,
+  per_page: '40',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: 'true',
+};
